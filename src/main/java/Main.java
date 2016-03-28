5aec85ff07f8547c809d85dd709b3d1bd5b0bde8
DavidMtz <df.martinez1@uniandes.edu.co>
2016-03-28 18:42:15 -0500

    mejora estilo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -68,51 +68,15 @@ public class Main {
 		result1 = (ArrayList<Double>) Statistics.antiLogaritList(result1);
 		result1 = (ArrayList<Double>) Statistics.roundDown(result1, 4);
 
-		ArrayList<Double> data2 = new ArrayList<Double>();
-		try {
-			data2 = (ArrayList<Double>) FileFinder.getData("test2.txt");
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		ArrayList<Double> log2 = (ArrayList<Double>) Statistics.logaritList(data2);
-		Double avg2 = Statistics.mean(log2);
-		Double var2 = Statistics.variance(data2);
-		Double dev2 = Math.sqrt(var2);
-		ArrayList<Double> result2 = new ArrayList<Double>();
-		result2.add(avg2 - (2 * dev2));
-		result2.add(avg2 - dev2);
-		result2.add(avg2);
-		result2.add(avg2 + dev2);
-		result2.add(avg2 + (2 * dev2));
-		result2 = (ArrayList<Double>) Statistics.antiLogaritList(result2);
-		result2 = (ArrayList<Double>) Statistics.roundDown(result2, 4);
-		String a1 = result1.get(0).toString();
-		String a2 = result1.get(1).toString();
-		String a3 = result1.get(2).toString();
-		String a4 = result1.get(3).toString();
-		String a5 = result1.get(4).toString();
-		String b1 = result2.get(0).toString();
-		String b2 = result2.get(1).toString();
-		String b3 = result2.get(2).toString();
-		String b4 = result2.get(3).toString();
-		// String b5 = result2.get(4).toString();
+		String a1 = "TEST1";
+		String a2 = "TEST2";
+		String a3 = "TEST3";
 
 		get("/", (request, response) -> {
 			Map<String, Object> attributes = new HashMap<>();
 			attributes.put("A1", a1);
 			attributes.put("A2", a2);
 			attributes.put("A3", a3);
-			attributes.put("A4", a4);
-			attributes.put("A5", a5);
-			attributes.put("B1", b1);
-			attributes.put("B2", b2);
-			attributes.put("B3", b3);
-			attributes.put("B4", b4);
-			attributes.put("B5", "prueba");
 
 			return new ModelAndView(attributes, "index.ftl");
 		}, new FreeMarkerEngine());
